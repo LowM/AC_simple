89016a794b14e893d2c1fea857a120c0db9ebeca
dlulaoyang <dlulaoyang@gmail.com>
2017-01-12 22:53:02 +0800

    Create GotoKey.java

---
+++ GotoKey.java
@@ -0,0 +1,13 @@
+//GotoKey.java
+package acTest;
+
+public class GotoKey {
+public int StartState = 0;
+public String ConvertStr = null;
+
+public GotoKey(int StartState, String ConvertStr)
+{
+this.StartState = StartState;
+this.ConvertStr = ConvertStr;
+}
+}
